3321f2a2093fa40db15fcb1f9c24634da725e96c
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-03-01 19:56:34 -0600

    Vista

---
+++ LABORATORIO1+AMY NOELIA LARA MENJIVAR/LABORATORIO1+AMY NOELIA LARA MENJIVAR/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using LABORATORIO1_AMY_NOELIA_LARA_MENJIVAR.Entidades;
+using LABORATORIO1_AMY_NOELIA_LARA_MENJIVAR.Negocio;
+
+Persona persona = new Persona();
+ClsPersona Clspersona= new ClsPersona();
+
+Console.WriteLine("Ingrese su nombre");
+persona.Nombre = (Console.ReadLine());
+
+Console.WriteLine("Ingrese su edad");
+persona.Edad = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese su sexo");
+persona.Sexo = (Console.ReadLine());
+
+Console.WriteLine("Ingrese peso en Kg");
+persona.Peso = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese altura en metros");
+persona.Altura = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(Clspersona.Calcular(persona));
+Console.WriteLine(Clspersona.MayorEdad(persona));
