741aa135d35618e52147b3df2564713ddfeb3918
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-03-01 19:56:10 -0600

    Metodos

---
+++ LABORATORIO1+AMY NOELIA LARA MENJIVAR/LABORATORIO1+AMY NOELIA LARA MENJIVAR/Negocio/ClsPersona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LABORATORIO1_AMY_NOELIA_LARA_MENJIVAR.Entidades;
+
+namespace LABORATORIO1_AMY_NOELIA_LARA_MENJIVAR.Negocio
+{
+    public class ClsPersona
+    {
+        public String Calcular(Persona per)
+        {
+            double pro = per.Peso / Math.Pow(per.Altura, 2);
+
+            if (pro > 25) 
+            {
+                return ("Tiene sobre peso");
+            }
+            else 
+            {
+            return ("Peso Ideal");
+            }
+        }
+        public String MayorEdad(Persona per)
+        {
+            if (per.Edad >= 18)
+            {
+                return ("Mayor de edad");
+            }
+            else
+            {
+                return ("Menor de edad");
+            }
+        }
+
+    }
+}
