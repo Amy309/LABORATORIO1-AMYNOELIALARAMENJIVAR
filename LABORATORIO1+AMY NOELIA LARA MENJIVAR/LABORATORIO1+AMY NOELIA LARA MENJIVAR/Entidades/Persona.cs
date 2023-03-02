75ace26991d8f45c67d7ddc78d4a1923d3e37115
Amy309 <125324963+Amy309@users.noreply.github.com>
2023-03-01 19:55:19 -0600

    Entidades

---
+++ LABORATORIO1+AMY NOELIA LARA MENJIVAR/LABORATORIO1+AMY NOELIA LARA MENJIVAR/Entidades/Persona.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO1_AMY_NOELIA_LARA_MENJIVAR.Entidades
+{
+    public class Persona
+    {
+        public String? Nombre { get; set; }
+        public int Edad { get; set; }
+        public String? Sexo { get; set;}
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+    }
+}
